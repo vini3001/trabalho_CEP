9543dbc633b802a1610a2bb7003cdb3c7c190eea
Vinícius Donizeti <donizetevinicius250@gmail.com>
2024-04-22 19:05:34 -0300

    Adicionar arquivos de projeto.

---
+++ trabalho_CEP/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using trabalho_CEP.Models;
+
+namespace trabalho_CEP.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+    }
+}
---
+++ trabalho_CEP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace trabalho_CEP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
